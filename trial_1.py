d80ac904374cf4dcf9fabeea433fa1d3a4892a51
aravind <arvindsuresh2009@gmail.com>
2015-12-30 21:03:29 +0530

    working on test.

--- trial_1.py
+++ trial_1.py
@@ -4,9 +4,12 @@ import sys
 import cv2
 
 # Ensuring correct arguments
-if not len(sys.argv) == 3:
-	print "Usage : %s <trainFile> <testPath>" % sys.argv[0]
+if not len(sys.argv) == 4:
+	print "Usage : %s <trainFile> <testFile> <outputDirectory>" % sys.argv[0]
 	sys.exit()
+
+# Constants
+dim = cv2.imread(trainList[0], 0).shape
 
 def loadListFromFile(filePath):
 	return map(lambda path: path.strip(), open(filePath, 'r').readlines())
@@ -14,7 +17,6 @@ def loadListFromFile(filePath):
 def train(trainList):
 	trainset = [ np.ravel(cv2.imread(path, 0)) for path in trainList ]
 	N = len(trainset)
-	dim = cv2.imread(trainList[0], 0).shape
 
 	# Computing mean image
 	mean = np.mean(trainset, 0)
@@ -25,20 +27,29 @@ def train(trainList):
 
 	w, v = np.linalg.eig(C)
 
-	return dim, mean, w, v
+	return mean, w, v
 
-def compute(img, dim, eiVecs):
-	imgR = cv2.resize(img, dim).ravel()
+def test(testList, eiVecs):
+	temp = map(lambda line: line.split(' '), testList)
+	testset = [ np.ravel(cv2.resize(cv2.imread(path, 0), dim)) for path in temp[:, 0]]
+	testLabels = temp[:, 1]
+
+	output = [ compute(img, eiVecs) for img in testset ]
+
+	# TODO : Compute loss and give a match
+	return output
+
+def compute(img, eiVecs):
 	coeff = map(lambda v: np.dot(v, imgR), eiVecs)
 	return coeff
 
 trainFile = sys.argv[1]
-testPath = sys.argv[2]
+testFile = sys.argv[2]
+outputDirectory = sys.argv[3]
 
 trainList = loadListFromFile(trainFile)
+testList = loadListFromFile(testFile)
 dim, mean, eiVals, eiVecs = train(trainList)
 
-img = cv2.imread(testPath, 0)
+img = cv2.imread(testFile, 0)
 coeff = compute(img, dim, eiVecs)
-
-print coeff
